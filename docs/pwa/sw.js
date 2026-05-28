ff65a6c97e9ce19984ba5c7a9fed1b74bf09100d
Po2432 <202832399+Po2432@users.noreply.github.com>
2026-05-28 17:45:43 +0300

    Update service worker to use relative paths

--- docs/pwa/sw.js
+++ docs/pwa/sw.js
@@ -1,39 +1,29 @@
-/*
- * Copyright (c) 2026 po2432
- * Repository: https://github.com/Po2432/ScribeTag
- */
-
+// Corrected /pwa/sw.js using relative paths
 const CACHE_NAME = 'scribetag-v1';
 const ASSETS = [
-    '/pwa/',
-    '/pwa/index.html',
-    '/pwa/manifest.json',
-    '/app.js',
-    '/style.css'
+    './',
+    './index.html',
+    './manifest.json',
+    '../app.js',
+    '../style.css'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(ASSETS))
-        .then(() => self.skipWaiting())
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
     );
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            );
-        })
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        caches.match(event.request).then(response => response || fetch(event.request))
     );
 });
